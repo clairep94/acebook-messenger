017e5cd0066f747804dc17d5acc2f0728864274c
ClairePeng <clairepeng94@gmail.com>
2023-11-15 15:30:15 +0000

    adds like count to Post but no button

--- api/models/post.js
+++ api/models/post.js
@@ -8,6 +8,10 @@ const PostSchema = new mongoose.Schema({
   },
   date_posted: {
     type: Date
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: []
   }
 });
 
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -3,6 +3,17 @@ import './Post.css';
 
 const Post = ({post}) => {
 
+  const formatLikes = (arr) => {
+    const numLikes = arr.length;
+
+    if (numLikes === 1){
+      return "1 like"
+    } else {
+      return `${numLikes} likes`
+    }}
+
+  const likes_formatted = formatLikes(post.likes)
+
   return(
     <article className='post-container' data-cy="post" key={ post._id }>
       {/* change below to display name later */}
@@ -10,6 +21,7 @@ const Post = ({post}) => {
       {/* change below to display date nicer later */}
       <p className='date-posted'>{ post.date_posted }</p>
       <p className='message'>{ post.message }</p>
+      <p className='likes'>{ likes_formatted }</p>
     </article>
   )
 }
