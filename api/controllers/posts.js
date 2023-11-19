aed7ee136371233c39779e60bec5334995ce6c65
ClairePeng <clairepeng94@gmail.com>
2023-11-19 13:00:41 +0000

    adds unlike feature and checking if session user has already liked

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -39,6 +39,7 @@ const PostsController = {
       res.status(201).json({ message: 'OK', token: token });
     });
   },
+
   Like: (req, res) => {
     // get the user_id & post_id:
     console.log("controllers/posts.js 44:")
@@ -47,21 +48,46 @@ const PostsController = {
     console.log(`Getting UserID: ${sessionUser}`)
     console.log(`Getting PostID: ${postID}`)
 
-    Post.findOneAndUpdate(
-      {_id: postID },
-      {$push: {likes: sessionUser}},
-      { new: true },
-      (err, updatedPost) => {
-        if (err) {
-          console.log('Unsuccessful Like in Post Controllers')
-          throw err;
-        } else {
-          console.log('Successful Like in Post Controllers')
-          const token = TokenGenerator.jsonwebtoken(req.user_id)
-          res.status(201).json({ message: 'Successful Like in Post Controllers', token: token, updatedPost });    
-        }
-      }
-    )
+    // Check if the user is already in the list of users who've liked this:
+    const alreadyLiked = Post.findOne({
+      $and: [
+        {_id: postID},
+        {likes: {$in: [sessionUser]}}
+      ]
+    }); // returns the matching doc or null
+
+    // If not already liked, adds sessionUser to list of likes:
+
+    if (!alreadyLiked) {
+      Post.findOneAndUpdate(
+        {_id: postID },
+        {$push: {likes: sessionUser}},
+        { new: true },
+        (err, updatedPost) => {
+          if (err) {
+            console.log('Unsuccessful Like in Post Controllers')
+            throw err;
+          } else {
+            console.log('Successful Like in Post Controllers')
+            const token = TokenGenerator.jsonwebtoken(req.user_id)
+            res.status(201).json({ message: 'Successful Like in Post Controllers', token: token, updatedPost });    
+          }})
+    } else {
+      Post.findOneAndUpdate(
+        {_id: postID },
+        {$pull: {likes: sessionUser}},
+        { new: true },
+        (err, updatedPost) => {
+          if (err) {
+            console.log('Unsuccessful Unlike in Post Controllers')
+            throw err;
+          } else {
+            console.log('Successful Unlike in Post Controllers')
+            const token = TokenGenerator.jsonwebtoken(req.user_id)
+            res.status(201).json({ message: 'Successful Unlike in Post Controllers', token: token, updatedPost });    
+          }})
+    }
+
   }
 };
 
