841c2df86ddc25ae98daf20409d87c66ef09794b
Sam Ford <samueljamesford@googlemail.com>
2023-11-19 17:55:19 +0000

    Adds logo and frontpage styling

--- frontend/src/pages/welcome.jsx
+++ frontend/src/pages/welcome.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import Navbar from '../components/navbar/navbar';
 import LogInForm from '../components/auth/LoginForm';
-const Welcome = ({navigate}) => {
+import '../pages/stylesheets/style.css';
 
-
+const Welcome = ({ navigate }) => {
   // Fetch user-specific data based on userId and render content accordingly
   // Example: Fetch user details, posts, etc. using userId
 
   return (
-    <div>
-    <Navbar/>
-      <h1>Welcome to Acebook</h1>
-      <p>Please Sign in</p> 
-      <LogInForm navigate={navigate}/>
-      {/* Render user-specific content */}
-      {/* You can fetch user data and display it here */}
-    </div>
+    <>
+      <Navbar />
+      <div className="wrap">
+        <div className="floatleft">
+          <div style={{ '--spacer-height': '20px' }} className="spacer"></div>
+          <p className='title'>WELCOME TO ACEBOOK</p>
+
+          <p>Connecting Ace individuals from around the world since 2023...</p>
+        </div>
+        <div className="floatright">
+          <div style={{ '--spacer-height': '100px' }} className="spacer"></div>
+          <p className='subtitles'>Log in to start connecting</p>
+          <LogInForm navigate={navigate}/>
+          <p className='subtitles'>Don't have an account?</p>
+          <button >Register</button>
+        </div>
+        <div style={{ clear: 'both' }}></div>
+      </div>
+    </>
   );
 };
 
 export default Welcome;
+
