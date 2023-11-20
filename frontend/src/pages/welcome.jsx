5d5f559c60efdc0060fa62891f4d073ec9a10acb
ClairePeng <clairepeng94@gmail.com>
2023-11-20 11:54:59 +0000

    adds popup box, will fill with login page elements

--- frontend/src/pages/welcome.jsx
+++ frontend/src/pages/welcome.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/navbar/navbar';
 import SignUpForm from '../components/user/SignUpForm';
 import '../pages/stylesheets/style.css';
 
+
 const Welcome = ({ navigate }) => {
   // Fetch user-specific data based on userId and render content accordingly
   // Example: Fetch user details, posts, etc. using userId
+
+  const [isLoginPopupVisible, setIsLoginPopupVisible] = useState(false);
+
+  const handleLoginPopupClick = () => {
+    setIsLoginPopupVisible(true);
+  }
+
+  const closeLoginPopup = () => {
+    setIsLoginPopupVisible(false);
+  }
+
 
   return (
     <>
@@ -21,8 +33,23 @@ const Welcome = ({ navigate }) => {
           <p className='subtitles'>Register an account to start connecting</p>
           <SignUpForm navigate={navigate}/>
           <p className='subtitles'>Already Registered?</p>
-          <a href='/login'><button className='Button'>Login</button></a>
+          {/* <a href='/login'><button className='Button' onClick={ handleLoginPopupClick }>Login</button></a> */}
+          <button className='Button' onClick={ handleLoginPopupClick }>Login</button>
         </div>
+
+        {isLoginPopupVisible && (
+        <div className="login-popup">
+          <div className="login-popup-content">
+            <span className="close" onClick={closeLoginPopup}>
+              &times;
+            </span>
+            <h2>Popup Content Goes Here</h2>
+            <p>This is a simple popup example.</p>
+          </div>
+        </div>
+      )}
+
+
         <div style={{ clear: 'both' }}></div>
       </div>
     </>
