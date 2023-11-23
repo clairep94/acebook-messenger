772c49b6097cac67c9716d37826617a26c0bc212
Sam Ford <63402941+Fordcois@users.noreply.github.com>
2023-11-23 10:45:35 +0000

    Update getNumLikesUserPreview.js

--- frontend/src/components/utility/getNumLikesUserPreview.js
+++ frontend/src/components/utility/getNumLikesUserPreview.js
@@ -7,13 +7,13 @@ const formatLikesUsersPreview = (arr, sessionUserID) => {
       return "Noone liked this"
     // 1 like
     } else if (numLikes === 1){ //Check if sessionUserID liked this
-      return (arr.some(user => user._id === sessionUserID) ? 'You liked this' : `${arr[0].fullName} liked this`);
+      return (arr.some(user => user._id === sessionUserID) ? 'You liked this' : `${arr[0].firstName} ${arr[0].lastName} liked this`);
     // 2 likes
     } else if (numLikes === 2){
-      return (arr.some(user => user._id === sessionUserID) ? 'You and 1 other liked this' : `${arr[0].fullName} and 1 other liked this`);
+      return (arr.some(user => user._id === sessionUserID) ? 'You and 1 other liked this' : `${arr[0].firstName} ${arr[0].lastName} and 1 other liked this`);
     // 3 or more likes
     } else {
-      return (arr.some(user => user._id === sessionUserID) ? `You and ${numLikes - 1} others liked this` : `${arr[0].fullName} and ${numLikes - 1} others liked this`);
+      return (arr.some(user => user._id === sessionUserID) ? `You and ${numLikes - 1} others liked this` : `${arr[0].firstName} ${arr[0].lastName} and ${numLikes - 1} others liked this`);
     }
   };
 
