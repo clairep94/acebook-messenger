dab65a9d2082aa17668e42f455eb2955b8744e33
ClairePeng <clairepeng94@gmail.com>
2023-11-28 19:22:08 +0000

    adds pfp to searchbar results

--- frontend/src/components/searchbar/SearchResult.js
+++ frontend/src/components/searchbar/SearchResult.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./SearchResult.css"
 
 
-export const SearchResult = ({result}) => {
+export const SearchResult = ({result, id}) => {
 
     return (
-    <div className="search-result">
+    <div className="search-result" key={id}>
         <a href={`/users/${result._id}`}>
-            {/* <img className="profilepic" src={`https://robohash.org/${result._id}.png`}></img> */}
-            {`${result.firstName} ${result.lastName}`}
+            {/* <img src={user.avatar} className='smallcirclemask' alt="Image Alt Text" /> */}
+            <img className='smallcirclemask' src={`https://picsum.photos/seed/${result._id}/300`}/>
+            <span className='who-liked-this-span'>{`${result.firstName} ${result.lastName}`}</span>
         </a>
     </div>
     )
